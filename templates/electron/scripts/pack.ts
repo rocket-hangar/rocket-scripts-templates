c50d6cdf92847df04c089566bd1423f86c93e30a
SSen <i@ssen.name>
2020-08-27 14:57:47 +0900

    feat: change electron packager to 'electron-builder'

--- templates/electron/scripts/pack.ts
+++ templates/electron/scripts/pack.ts
@@ -1,19 +1,85 @@
 import { rimraf } from '@ssen/promised';
-import packager from 'electron-packager';
+import { build, Configuration, Platform } from 'electron-builder';
+import fs from 'fs-extra';
 import path from 'path';
+
+const appId: string = 'myorg.appid';
 
 (async () => {
   await rimraf(path.join(process.cwd(), 'apps'));
-  
-  const appPaths = await packager({
-    dir: path.join(process.cwd(), 'out/app'),
-    out: path.join(process.cwd(), 'apps/app'),
-    // if you are on macos
-    //    you have to install wine to build win32
-    //    brew cask install xquartz wine-stable
-    // @see https://electron.github.io/electron-packager/master/#related
-    platform: ['darwin', 'linux', 'win32'],
+
+  await fs.copy(
+    path.join(process.cwd(), 'src/app/package.json'),
+    path.join(process.cwd(), 'out/app/package.json'),
+  );
+
+  const config: Configuration = {
+    electronVersion: require('electron/package.json').version,
+    appId,
+    productName: 'MyApp',
+    directories: {
+      buildResources: path.join(process.cwd(), 'buildResources'),
+      output: path.join(process.cwd(), 'apps/app'),
+    },
+  };
+
+  const mac = build({
+    targets: Platform.MAC.createTarget(),
+    projectDir: path.join(process.cwd(), 'out/app'),
+    config: {
+      ...config,
+      mac: {
+        // @see https://developer.apple.com/documentation/bundleresources/information_property_list/lsapplicationcategorytype
+        category: 'public.app-category.utilities',
+      },
+      // Enable when you ready to Apple Notarization
+      //afterSign: async ({ appOutDir, packager }) => {
+      //  if (!process.env.APPLE_DEVELOPER_ID || !process.env.APPLE_DEVELOPER_PASSWORD) {
+      //    throw new Error(`Undefiend $APPLE_DEVELOPER_ID and $APPLE_DEVELOPER_PASSWORD`);
+      //  }
+      //
+      //  console.log(`🚀 Start Apple Notarize...`);
+      //
+      //  // import { notarize } from 'electron-notarize'
+      //  return await notarize({
+      //    appBundleId: appId,
+      //    appPath: `${appOutDir}/${packager.appInfo.productFilename}.app`,
+      //    appleId: process.env.APPLE_DEVELOPER_ID,
+      //    appleIdPassword: process.env.APPLE_DEVELOPER_PASSWORD,
+      //  });
+      //},
+    },
   });
-  
+
+  const win = build({
+    targets: Platform.WINDOWS.createTarget(),
+    projectDir: path.join(process.cwd(), 'out/app'),
+    config: {
+      ...config,
+      win: {
+        target: {
+          target: 'nsis',
+          arch: ['x64', 'ia32'],
+        },
+      },
+      nsis: {
+        perMachine: true,
+      },
+    },
+  });
+
+  const linux = build({
+    targets: Platform.LINUX.createTarget(),
+    projectDir: path.join(process.cwd(), 'out/app'),
+    config: {
+      ...config,
+      linux: {
+        target: ['deb'],
+      },
+    },
+  });
+
+  const appPaths = await Promise.all([mac, win, linux]).then(([mac, win, linux]) => ({ mac, win, linux }));
+
   console.log('👍 Build successfully!', appPaths);
 })();
